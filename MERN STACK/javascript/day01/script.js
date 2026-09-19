b72c64abe28c6821b93a74b53fc4fcf86ff09a13
Nikhil296 <nikhilchakrwarti2906@gmail.com>
2026-09-19 18:22:12 +0530

    Delete MERN STACK directory

--- MERN STACK/javascript/day01/script.js
+++
@@ -1,9 +0,0 @@
-console.log("strike is coming");
-// introduction of node js
-// what is node js 
-// how a java script file run 
-// there is c++ code behind run any javascript file
-// which is present in node js
-// installation of node js
-// installation is completed
-console.log("strike is coming");
