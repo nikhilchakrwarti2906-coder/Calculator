9a5794838d24269bd3c45893f0cedd698a6131df
Nikhil296 <nikhilchakrwarti2906@gmail.com>
2026-09-19 18:22:23 +0530

    Delete aULTIMATE JAVASCRIPT TUTORIAL directory

--- aULTIMATE JAVASCRIPT TUTORIAL/01/script.js
+++
@@ -1,3 +0,0 @@
-// this is external linking
-console.log("Hello world");
-console.log("MY name is Nikhl chakrwarti");
--- aULTIMATE JAVASCRIPT TUTORIAL/03/script.js
+++
@@ -1,5 +0,0 @@
-// this is about functions
-function multiply(a,b){
-    return(a*b);
-}
-console.log(multiply(5,5));
